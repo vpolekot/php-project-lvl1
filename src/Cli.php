d20dfee97a302fc26d29ce7cb3c563e68f2ee116
vpolekot <splinter0t@gmail.com>
2021-01-26 18:39:19 +0200

    3rd step

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function getUserName(){
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
