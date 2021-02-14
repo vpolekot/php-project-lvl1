1f17a25fcd787f4b598de08fb4e83262b85acc5d
vpolekot <splinter0t@gmail.com>
2021-02-14 15:55:16 +0200

    added brain-even logic

---
+++ src/Even.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Comments
+ * PHP version 7.4
+ * 
+ * @category Function
+ * @package  Brain-Even
+ * @author   author <aaa@email.com>
+ * @license  License 
+ * @link     http://url.com
+ */
+namespace Brain\Games\Even;
+
+//use Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+/**
+ * Undocumented function
+ *
+ * @param string $username user's name
+ * 
+ * @return void
+ */
+function countAnswer($username)
+{
+    $count_correct_answers = 0;
+
+    while ($count_correct_answers <= 3) {
+        
+        if ($count_correct_answers == 3) {
+            line("Congratulations, {$username}!");
+            break;
+        }
+        if (askUser($username)) {
+            $count_correct_answers += 1;
+        }
+    }
+}
+
+/**
+ * Undocumented function
+ *
+ * @param string $username user's name
+ * 
+ * @return boolean 
+ */
+function askUser($username)
+{
+    $question_number = rand(0, 100);
+    $question_number % 2 ? $correct_answer = "yes" :  $correct_answer = "no";
+
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    line('Question: ', $question_number);
+    $user_answer = prompt('Your answer:');
+    if ($user_answer != 'yes' 
+        && $user_answer != 'no' 
+        && $user_answer != $correct_answer
+    ) {
+        line("'{$user_answer}' is wrong answer ;(. Correct answer was '{$correct_answer}'");
+        line("Let's try again, {$username}!");
+        return false;
+    } else {
+        line("Correct!");
+        return true;
+    }
+}
