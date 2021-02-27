5167bfcf19949dd82777f90917f861d5395357ac
vpolekot <splinter0t@gmail.com>
2021-02-27 14:46:58 +0200

    fixed correct choice for Even

--- src/Calc.php
+++ src/Calc.php
@@ -94,8 +94,7 @@ function askUser()
     line("Correct answer {$correct_answer}");
 
     $user_answer = prompt('Your answer');
-    if ($user_answer != $correct_answer
-    ) {
+    if ($user_answer != $correct_answer) {
         line("'{$user_answer}' is wrong answer ;(. Correct answer was '{$correct_answer}'");
         return false;
     } else {
--- src/Even.php
+++ src/Even.php
@@ -81,9 +81,7 @@ function askUser()
     //line("Correct answer {$correct_answer}");
 
     $user_answer = prompt('Your answer');
-    if ($user_answer != 'yes' && $user_answer != 'no' 
-        || $user_answer != $correct_answer
-    ) {
+    if ($user_answer != $correct_answer) {
         line("'{$user_answer}' is wrong answer ;(. Correct answer was '{$correct_answer}'");
         return false;
     } else {
