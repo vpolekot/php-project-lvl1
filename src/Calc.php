9bd1d8b09480e6803e05eadab5b373c177306b2b
vpolekot <splinter0t@gmail.com>
2021-02-27 14:22:05 +0200

    added brain-calc to /bin, composer.json and Makefile

---
+++ src/Calc.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Comments
+ * PHP version 7.4
+ * 
+ * @category Function
+ * @package  Brain-Calc
+ * @author   author <aaa@email.com>
+ * @license  License 
+ * @link     http://url.com
+ */
+namespace Brain\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+/**
+ * Undocumented function
+ *
+ * @return void
+ */
+function playEven()
+{
+    $name = getUserName();
+    countAnswer($name); 
+}
+
+/**
+ * Undocumented function
+ *
+ * @return void
+ */
+function getUserName()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    return $name;
+}
+
+
+/**
+ * Undocumented function
+ *
+ * @param string $username user's name
+ * 
+ * @return void
+ */
+function countAnswer($username)
+{
+    $count_correct_answers = 0;
+
+    while ($count_correct_answers <= 3) {
+        if ($count_correct_answers == 3) {
+            line("Congratulations, {$username}!");
+            break;
+        }
+        if (askUser($username)) {
+            $count_correct_answers += 1;
+        } else {
+            line("Let's try again, {$username}!");
+            break;
+        }
+    }
+}
+
+/**
+ * Undocumented function
+ *
+ * @return boolean 
+ */
+function askUser()
+{
+    $question_number = rand(0, 100);
+    $question_number % 2 == 0 ? $correct_answer = "yes" :  $correct_answer = "no";
+    
+    line("Question: {$question_number}");
+    //line("Correct answer {$correct_answer}");
+
+    $user_answer = prompt('Your answer');
+    if ($user_answer != 'yes' && $user_answer != 'no' 
+        || $user_answer != $correct_answer
+    ) {
+        line("'{$user_answer}' is wrong answer ;(. Correct answer was '{$correct_answer}'");
+        return false;
+    } else {
+        line("Correct!");
+        return true;
+    }
+}
